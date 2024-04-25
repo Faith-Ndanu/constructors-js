156a6e9ac6801a98c1fe56a3682906b352b9cfe3
Faith-Ndanu <faithndanumunyao@gmail.com>
2024-04-25 07:12:56 +0300

    added file

---
+++ classes.js
@@ -0,0 +1,38 @@
+
+class Car{
+    constructor(make,model,year,isAvailable){
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.isAvailable = isAvailable
+    }
+    toggleAvailability(){
+        this.isAvailable=!isAvailable
+    }
+}
+    const car1= new Car('mazda','Demio',2020,true);
+    console.log(car1.isAvailable);
+
+
+class Rental{
+    constructor(rentersName,car,startDate,endDate){
+this.rentersName = "Kesa"
+this.car = "Audi"
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
